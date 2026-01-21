ccfb75d00461aec2b1e871a820d38fad4921080a
Danil <bd986650@gmail.com>
2026-01-21 19:30:05 +0700

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -36,11 +36,7 @@ module.exports = {
             presets: ['@babel/preset-env']
           },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
+      }
     ],
   },
 };
